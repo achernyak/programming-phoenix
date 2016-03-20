0418f5b7d81a45bdf82c99707cabdce78cc94aef
achernyak <artemchernyak@gmail.com>
2016-03-20 12:38:58 -0500

    auth tests

---
+++ rumbl/test/controllers/auth_test.exs
@@ -0,0 +1,20 @@
+defmodule Rumbl.AuthTest do
+  use Rumbl.ConnCase
+  alias Rumbl.Auth
+
+  test "authenticate_user halts when no current_user exists",
+  %{conn: conn} do
+    conn = Auth.authenticate_user(conn, [])
+    assert conn.halted
+  end
+
+  test "authenticate_user continues when the current_user exists",
+  %{conn: conn} do
+    conn =
+      conn
+      |> assign(:cuurent_user, %Rumbl.User{})
+      |> Auth.authenticate_user([])
+
+    refute conn.halted
+  end
+end
